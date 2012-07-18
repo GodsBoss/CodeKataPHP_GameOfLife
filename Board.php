6c9313840c891964c8e22d64a05eae12e3a021a3
Timo Reitz <godsboss@gmx.de>
2012-07-18 18:59:02 +0200

    Board has a size.

---
+++ Board.php
@@ -0,0 +1,18 @@
+<?php
+
+class Board{
+	private $width;
+	private $height;
+
+	public static function create($width, $height){
+		return new Board($width, $height);}
+
+	public function __construct($width, $height){
+		$this->width = $width;
+		$this->height = $height;}
+
+	public function getWidth(){
+		return $this->width;}
+
+	public function getHeight(){
+		return $this->height;}}
--- ConwayTest.php
+++ ConwayTest.php
@@ -1,3 +1,17 @@
 <?php
 
-class ConwayTest extends PHPUnit_Framework_TestCase{}
+require_once("Board.php");
+
+class ConwayTest extends PHPUnit_Framework_TestCase{
+
+	public function testBoardHasASize(){
+		$this->givenBoardWithSize(5, 3);
+		$this->thenBoardHasSize(5, 3);}
+
+	private function thenBoardHasSize($width, $height){
+		$this->assertEquals($width, $this->board->getWidth());}
+
+	private function givenBoardWithSize($width, $height){
+		$this->board = Board::create($width, $height);}
+
+	private $board;}
